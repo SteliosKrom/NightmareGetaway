ffda7389bece1acae9372dd02a53687b86b0f711
SteliosKrom <stelios.yolokrom@gmail.com>
2024-07-17 00:08:51 +0300

    Commit

--- Escape Creepy House/Assets/Scripts/Game/FootstepsSystem.cs
+++ Escape Creepy House/Assets/Scripts/Game/FootstepsSystem.cs
@@ -8,9 +8,12 @@ public class FootstepsSystem : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
         {
-            footstepsAudioSource.enabled = true;
-            footstepsAudioSource.pitch = Random.Range(0.5f, 1.5f);
-            footstepsAudioSource.volume = 0.2f;
+            if (RoundManager.instance.currentState == GameState.playing)
+            {
+                footstepsAudioSource.enabled = true;
+                footstepsAudioSource.pitch = Random.Range(0.5f, 1.5f);
+                footstepsAudioSource.volume = 0.2f;
+            }
         }
         else
         {
--- Escape Creepy House/Assets/Scripts/Menu/SettingsManager.cs
+++ Escape Creepy House/Assets/Scripts/Menu/SettingsManager.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class SettingsManager : MonoBehaviour
 {
     [Header("TYPES")]
-    private const string masterVolume = "MasterVolume";
-    private const string gameMusicVolume = "GameMusicVolume";
-    private const string sfxVolume = "SoundEffectsVolume";
+    private const string masterVol = "MasterVolume";
+    private const string gameMusicVol = "GameMusicVolume";
+    private const string sfxVol = "SoundEffectsVolume";
 
     [Header("CLASSES")]
     [SerializeField] private AudioMixer myAudioMixer;
@@ -17,20 +17,35 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] private Slider mainGameVolumeSlider;
     [SerializeField] private Slider sfxVolumeSlider;
 
-    //Values that are displayed at the right of the volume sliders
     [SerializeField] private TextMeshProUGUI masterValueText; 
     [SerializeField] private TextMeshProUGUI gameMusicValueText;
-    [SerializeField] private TextMeshProUGUI sfxValueText;  
+    [SerializeField] private TextMeshProUGUI sfxValueText;
 
-    public void MasterVolumeSlider(float masterVolume)
+    private void Start()
     {
-        masterVolume = masterVolumeSlider.value;
-        masterValueText.text = masterVolume.ToString("0.0");
+        myAudioMixer.SetFloat(masterVol, 0f);
+        myAudioMixer.SetFloat(gameMusicVol, 0f);
+        myAudioMixer.SetFloat(sfxVol, 0f);
     }
 
-    public void GameVolumeSlider(float gameVolume)
+    public void MasterVolumeSlider()
     {
-        gameVolume = mainGameVolumeSlider.value;
+        float masterVolume = masterVolumeSlider.value;
+        masterValueText.text = masterVolume.ToString("0.0");
+        myAudioMixer.SetFloat(masterVol, Mathf.Log10(masterVolume) * 20);
+    }
+
+    public void GameVolumeSlider()
+    {
+        float gameVolume = mainGameVolumeSlider.value;
         gameMusicValueText.text = gameVolume.ToString("0.0");
+        myAudioMixer.SetFloat(gameMusicVol, Mathf.Log10(gameVolume) * 20);
+    }
+
+    public void SFXVolumeSlider()
+    {
+        float sfxVolume = sfxVolumeSlider.value;
+        sfxValueText.text = sfxVolume.ToString("0.0");
+        myAudioMixer.SetFloat(sfxVol, Mathf.Log10(sfxVolume) * 20);
     }
 }
