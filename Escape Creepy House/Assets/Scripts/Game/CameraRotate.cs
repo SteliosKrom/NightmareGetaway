afa4c66da3b56b11c9184ee5450090a0f209dc1a
SteliosKrom <stelios.yolokrom@gmail.com>
2024-07-22 23:39:07 +0300

    Commit

--- Escape Creepy House/Assets/Scripts/Game/CameraRotate.cs
+++ Escape Creepy House/Assets/Scripts/Game/CameraRotate.cs
@@ -9,6 +9,7 @@ public class CameraRotate : MonoBehaviour
     [SerializeField] private Slider sensitivitySlider;
     [SerializeField] private TextMeshProUGUI sensitivityValueText;
     [SerializeField] private Transform spotlight;
+    [SerializeField] private Transform mainCamera;
 
     [Header("TYPES")]
     [SerializeField] private float minX;
@@ -17,23 +18,33 @@ public class CameraRotate : MonoBehaviour
     [SerializeField] private float sensX;
 
     private float defaultSensitivity = 1f;
-    private float spotlightRotationSpeed = 2.5f;
+    private float spotlightRotationSpeed = 6f;
+    private float mainCameraRotationSpeed = 2f;
+
     private float xRotation;
     private float yRotation;
     private float zRotation;
+
     private float spotlightYRotation;
     private float spotlightXRotation;
+
+    private float mainCameraYRotation;
+    private float mainCameraXRotation;
 
     private void Start()
     {
         xRotation = 0f;
         yRotation = -90f;
         zRotation = 0f;
+
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
         sensitivitySlider.value = defaultSensitivity;
 
         spotlightYRotation = yRotation;
         spotlightXRotation = xRotation;
+
+        mainCameraYRotation = yRotation;
+        mainCameraXRotation = xRotation; 
     }
 
     private void Update()
@@ -63,6 +74,7 @@ public class CameraRotate : MonoBehaviour
             playerRotate.RotatePlayer(yRotation);
 
             UpdateSpotlightRotation(yRotation, xRotation);
+            UpdateCameraRotation(yRotation, xRotation);
         }
     }
 
@@ -71,6 +83,13 @@ public class CameraRotate : MonoBehaviour
         spotlightYRotation = Mathf.Lerp(spotlightYRotation, newYRotation, spotlightRotationSpeed * Time.deltaTime);
         spotlightXRotation = Mathf.Lerp(spotlightXRotation, newXRotation, spotlightRotationSpeed * Time.deltaTime);
         spotlight.rotation = Quaternion.Euler(spotlightXRotation, spotlightYRotation, zRotation);
+    }
+
+    public void UpdateCameraRotation(float newYRotation, float newXRotation)
+    {
+        mainCameraYRotation = Mathf.Lerp(mainCameraYRotation, newYRotation, mainCameraRotationSpeed * Time.deltaTime);
+        mainCameraXRotation = Mathf.Lerp(mainCameraXRotation, newXRotation, mainCameraRotationSpeed * Time.deltaTime);
+        mainCamera.rotation = Quaternion.Euler(mainCameraXRotation, mainCameraYRotation, zRotation);
     }
 
     public void SetInitialRotation(float initialYRotation)
